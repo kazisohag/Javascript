59bcd156f75bcd7e2a94d2a020172e01a24c9666
kazisohag <sohag15-543@diu.edu.bd>
2020-10-23 23:34:37 +0600

    Math.js &date.js Submit

---
+++ math.js
@@ -0,0 +1,17 @@
+var e = Math.E
+var Pie = Math.PI
+console.log(" ")
+console.log("e = " + e)
+console.log("Pie = " + Pie)
+
+var n = 5.674
+console.log(Math.abs(n))
+console.log(Math.floor(n))
+console.log(Math.ceil(n))
+console.log(Math.round(n))
+console.log(Math.max(900,10000))
+console.log(Math.min(2000,400))
+console.log(Math.log(n))
+console.log(Math.pow(2,4))
+console.log(Math.round(Math.random() * 20 + 1))
+console.log(" ")

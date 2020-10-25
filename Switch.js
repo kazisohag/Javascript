01c03aac22b1fb3f9c0119aa63be3fc3f9b2463e
kazisohag <sohag15-543@diu.edu.bd>
2020-10-26 02:19:36 +0600

    Switch.js Submmit

---
+++ Switch.js
@@ -0,0 +1,78 @@
+var date = new Date()
+var time = date.getHours()
+
+switch(time){
+    case 0:
+        console.log(time.toString() + " Happy Mid Night")
+        break
+    case 1:
+        console.log(time.toString() + " Happy Mid Night")
+        break
+    case 2:
+        console.log(time.toString() + " Happy Mid Night")
+        break
+    case 3:
+        console.log(time.toString() + " Happy Early Morning")
+        break
+    case 4:
+        console.log(time.toString() + " Happy Mid Night")
+        break
+    case 5:
+        console.log(time.toString() + " Happy Mid Night")
+        break
+    case 6:
+        console.log(time.toString() + " Good Morning")
+        break
+    case 7:
+         console.log(time.toString() + " Good Morning")
+        break
+    case 8:
+        console.log(time.toString() + " Good Morningt")
+        break
+    case 9:
+        console.log(time.toString() + " Good Morning")
+        break
+    case 10:
+        console.log(time.toString() + " Good Morning")
+        break
+    case 11:
+        console.log(time.toString() + " Good Morning")
+        break
+    case 12:
+        console.log(time.toString() + " Good After Noon")
+        break
+    case 13:
+        console.log(time.toString() + " Good After Noon")
+        break
+    case 14:
+        console.log(time.toString() + " Good After Noon")
+        break
+    case 15:
+        console.log(time.toString() + " Good After Noon")
+        break
+    case 16:
+        console.log(time.toString() + " Good After Noon")
+        break
+    case 17:
+        console.log(time.toString() + " Good Evening")
+        break
+    case 18:
+        console.log(time.toString() + " Good Evening")
+        break
+    case 19:
+        console.log(time.toString() + " Good Evening")
+        break
+    case 20:
+        console.log(time.toString() + " Good Night")
+        break
+    case 21:
+        console.log(time.toString() + " Good Night")
+        break
+    case 22:
+        console.log(time.toString() + " Good Night")
+        break
+    case 23:
+        console.log(time.toString() + " Good Night")
+        break
+    
+}

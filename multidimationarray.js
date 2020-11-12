74d1e186020ade8d31ff6429fc8bb974621d5091
kazisohag <sohag15-543@diu.edu.bd>
2020-11-12 23:24:00 +0600

    MultiDimantion Array

---
+++ multidimationarray.js
@@ -0,0 +1,13 @@
+var mList = [
+    [5,6,3,2],
+    [9,3,6,4],
+    [12,56,72,8]
+]
+
+
+for(var i = 0; i<mList.length; i++){
+    for(var j = 0; j<mList[i].length; j++){
+        console.log('\n Row ' + i + ' & Clomn ' + j + ' :' + mList[i][j] )
+        
+    }
+}
